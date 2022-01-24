97712834d03ce703fbda9d4f1481276c8764e7fb
parsoss <85831713+parsoss@users.noreply.github.com>
2022-01-24 11:45:53 +0900

    Rename 22.1.24/2.c to 2022/2.c

--- 2022/2.c
+++ 2022/2.c
@@ -10,15 +10,15 @@ int main()
     gop = a*b;
 
     if(a<b){
-        printf("Ã¹ ¹øÂ° ¼ö: %d\n", a);
-        printf("µÎ ¹øÂ° ¼ö: %d\n", b);
+        printf("ì²« ë²ˆì§¸ ìˆ˜: %d\n", a);
+        printf("ë‘ ë²ˆì§¸ ìˆ˜: %d\n", b);
         for(i=a+1; i<b; i++){
             gop = gop*i;
         }
     }
     else{
-        printf("Ã¹ ¹øÂ° ¼ö: %d\n", a);
-        printf("µÎ ¹øÂ° ¼ö: %d\n", b);
+        printf("ì²« ë²ˆì§¸ ìˆ˜: %d\n", a);
+        printf("ë‘ ë²ˆì§¸ ìˆ˜: %d\n", b);
         for(i=b+1; i<a; i++){
             gop = gop*i;
         }
