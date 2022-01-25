a05ed921781d864113e976b3d5254660edde9795
parsoss <85831713+parsoss@users.noreply.github.com>
2022-01-25 15:06:42 +0900

    Update 2.c

--- 2022/2.c
+++ 2022/2.c
@@ -10,15 +10,15 @@ int main()
     gop = a*b;
 
     if(a<b){
-        printf("첫 번째 수: %d\n", a);
-        printf("두 번째 수: %d\n", b);
+        printf("first number: %d\n", a);
+        printf("second number: %d\n", b);
         for(i=a+1; i<b; i++){
             gop = gop*i;
         }
     }
     else{
-        printf("첫 번째 수: %d\n", a);
-        printf("두 번째 수: %d\n", b);
+        printf("first number: %d\n", a);
+        printf("second number: %d\n", b);
         for(i=b+1; i<a; i++){
             gop = gop*i;
         }
